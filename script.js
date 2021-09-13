b22901a08c15b186b58f60568695f1a34e0509b7
Austin Pratt <hargis32@gmail.com>
2021-09-13 15:03:58 -0500

    added working time and date display

---
+++ script.js
@@ -0,0 +1,8 @@
+var timeDisplayEl = $('#currentDay');
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+}
+
+setInterval(displayTime, 1000);
